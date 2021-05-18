53b4c5f1811ed2f90b9f572e77f9f0986b3f3894
Technical Priyam <priyamvadofficial1@gmail.com>
2021-05-18 06:07:17 +0530

    completed functionality of formula bar

--- browser_excel/activity/init.js
+++ browser_excel/activity/init.js
@@ -1,36 +1,46 @@
-let column = 26;
+// initially grid build
+let columns = 26;
 let rows = 100;
-
+// 65
 let topRow = document.querySelector(".top-row");
 let leftCol = document.querySelector(".left-col");
 let grid = document.querySelector(".grid");
-
-for(let i=0;i < columns;i++)
-{
+for (let i = 0; i < columns; i++) {
     let div = document.createElement("div");
-    div.innerText = String.fromCharCode(65+i);
-    div.setAttribute("class","cell");
+    div.innerText = String.fromCharCode(65 + i);
+    div.setAttribute("class", "cell");
     topRow.appendChild(div);
 }
-
-for(let i =1;i <= rows;i++)
-{
-    let div=document.createElement("div");
-    div.innerText=i;
-    div.setAttribute("class","block");
+for (let i = 1; i <= rows; i++) {
+    let div = document.createElement("div");
+    div.innerText = i
+    div.setAttribute("class", "block");
     leftCol.appendChild(div);
 }
-
-for(let i=0;i < rows;i++)
-{
-    let row=document.createElement("div");
-    row.setAttribute("class","row");
-    for(let j=0;j<columns;j++)
-    {
-        let div = document.createElement("div");
-        div.innerText=`${i}${String.fromCharCode(65+j)}`;
-        div.setAttribute("class","cell");
-        row.appendChild(div);
+let sheetArr = [];
+// rows-> 100
+// col - > 26
+for (let i = 0; i < rows; i++) {
+    let row = document.createElement("div");
+    let rowArr = [];
+    row.setAttribute("class", "row");
+    for (let j = 0; j < columns; j++) {
+        // UI
+        let cell = document.createElement("div");
+        cell.setAttribute("class", "cell");
+        cell.setAttribute("rid", i);
+        cell.setAttribute("cid", j);
+        cell.setAttribute("contenteditable", "true");
+        row.appendChild(cell);
+        // 
+        let cellObj = {
+            isBold: false,
+            isItalic:false,
+            isUnderline:false
+        }
+        rowArr.push(cellObj);
     }
     grid.appendChild(row);
+    sheetArr.push(rowArr);
 }
+console.log(sheetArr)
